3716d58718a8f272428289ca744d409eac23c377
M0608766 <laurin.schwander@mgb.ch>
2024-02-08 15:44:06 +0100

    implemented getTodos with gin gonic

--- main.go
+++ main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/yutive/todo-crud-api/models"
+	"net/http"
 )
 
+var todo []models.Todo
+
+func HomeHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "todo crud api",
+	})
+}
+
+func GetTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todo)
+}
+
 func main() {
-	fmt.Println("first gin gonic API")
+	//starter text
+	fmt.Println("todo crud api")
+
+	//start gin server
+	r := gin.Default()
+
+	//	db
+	todo = append(todo, models.Todo{ID: "0", Text: "wash dishes"})
+	todo = append(todo, models.Todo{ID: "1", Text: "watch football"})
+
+	//Home Handler
+	r.GET("/", HomeHandler)
+
+	//get Todos
+	r.GET("/todos", GetTodos)
+
+	// listen and serve on localhost:8080
+	r.Run()
 }
